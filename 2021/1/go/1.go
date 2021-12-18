df619985b8dcac1e797a0761b51d75f4c0964884
Aliaksandr Anufryieu <aliaksandr.anufryieu@gmail.com>
2021-12-18 23:30:25 +0300

    2021 - 1.2

--- 2021/1/go/1.go
+++ 2021/1/go/1.go
@@ -1,15 +1,15 @@
 package main
 
-import (	
+import (
 	"fmt"
 	"log"
-	"os"	
-	"strings"
+	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
-	data, err := os.ReadFile("../input.txt")
+	data, err := os.ReadFile("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -23,19 +23,23 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		
+
 		values[index] = value
 	}
 
-	fmt.Println(first(values))
+	fmt.Println("How many measurements are larger than the previous measurement?")
+	fmt.Println(count_of_values_that_larger_than_previous(values))
+
+	fmt.Println("How many sums are larger than the previous sum?")
+	fmt.Println(count_of_values_that_larger_than_previous(sums(values)))
 }
 
-func first(values []int) int {
+func count_of_values_that_larger_than_previous(values []int) int {
 	var previosValue int
 	count := 0
 
 	for index, value := range values {
-		if (index > 0 && value > previosValue) {
+		if index > 0 && value > previosValue {
 			count++
 		}
 
@@ -43,4 +47,14 @@ func first(values []int) int {
 	}
 
 	return count
+}
+
+func sums(values []int) []int {
+	sums := make([]int, len(values)-2)
+
+	for i := 0; i < len(values)-2; i++ {
+		sums[i] = values[i] + values[i+1] + values[i+2]
+	}
+
+	return sums
 }
